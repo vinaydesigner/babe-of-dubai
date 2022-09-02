1f51dbf43004a4afe88f0309278d16457472d396
vinaydesigner <vinaylogicsofts@gmail.com>
2022-09-02 17:10:07 +0530

    get API and Post AIP working in this backup

--- pages/reviews.js
+++ pages/reviews.js
@@ -57,7 +57,7 @@ const review = ({ rqpage, remetas }) => {
                   </>
                 )
 
-              })};
+              })}
             </Col>
           </Row>
         </Container>
