af37e2773a50ec145caf029d4126ad3ff797981e
YashLogicsofts <yash@logicsofts.com>
2022-09-14 18:34:15 +0530

    booking model thumb selection done

--- src/api/bookingapi.js
+++ src/api/bookingapi.js
@@ -1,14 +1,29 @@
-import axios from "axios"
+import axios from "axios";
 
 export const bookingForm = async (data) => {
-    return axios.post(`https://dev.havingado.net/babesofdubai/api/v1/booking-form`, data)
-        .then((response) => {
-            if (response.status === 20 || response.status === 201) {
-                return response.data;
-            }
-            // console.log(data);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+  return axios
+    .post(`https://dev.havingado.net/babesofdubai/api/v1/booking-form`, data)
+    .then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        return response.data;
+      }
+      // console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+};
+
+export const BookingSelect = async (data) => {
+  return axios
+    .get("https://dev.havingado.net/babesofdubai/api/v1/escort-list")
+    .then((response) => {
+      if (response.status === 200 || response.status === 201) {
+        return response.data;
+      }
+      // console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 };
